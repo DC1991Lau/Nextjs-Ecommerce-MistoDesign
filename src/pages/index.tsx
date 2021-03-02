72c2d2054e32b714dcddfb7b3f43c0c4a587cbf3
DC1991Lau <diogocosta791@gmail.com>
2021-03-02 16:11:57 +0000

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+const Home: NextPage<AppProps> = () => {
+  return (
+    <div>
+      <h1>MISTO ECOMMERCE</h1>
+    </div>
+  )
+}
+
+export default Home
